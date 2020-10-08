af2a4ef9e1490d4cc2d8b229b6d5b806a71bada2
LucasSecret <lucas.pierre.secret@gmail.com>
2020-10-08 21:06:05 +0200

    Reimplement lost Point methods

--- Draw3D/Point.cpp
+++ Draw3D/Point.cpp
@@ -7,21 +7,59 @@ Point::Point()
 {
 	this->xAxis = 0;
 	this->yAxis = 0;
-	numberOfpoint++;
+	this->zAxis = 0;
+	this->color = BLUE;
+}
+
+Point::Point(float x, float y, float z)
+{
+	this->xAxis = x;
+	this->yAxis = y;
+	this->zAxis = z;
+	this->color = BLUE;
+}
+
+Point::Point(float x, float y, float z, int color)
+{
+	this->xAxis = x;
+	this->yAxis = y;
+	this->zAxis = z;
+	this->color = color;
+}
+
+Point::Point(float x, float y, int color)
+{
+	this->xAxis = x;
+	this->yAxis = y;
+	this->zAxis = 0;
+	this->color = color;
 }
 
 Point::Point(float x, float y)
 {
 	this->xAxis = x;
 	this->yAxis = y;
-	numberOfpoint++;
+	this->zAxis = 0;
+	this->color = BLUE;
+}
+
+Point::Point(Point& copy)
+{
+	xAxis = copy.xAxis;
+	yAxis = copy.yAxis;
+	zAxis = copy.zAxis;
+	color = copy.color;
 }
 
 float Point::getXaxis() { return xAxis; }
 float Point::getYaxis() { return yAxis; }
+float Point::getZaxis() { return zAxis; }
+int   Point::getColor() { return color; }
 
 void Point::setXaxis(float x) { this->xAxis = x; }
 void Point::setYaxis(float y) { this->yAxis = y; }
+void Point::setZaxis(float z) { this->zAxis = z; }
+void Point::setColor(int y)   { this->color = color; }
 
 
 
@@ -29,41 +67,109 @@ void Point::homotethia(double ratio)
 {
 	xAxis *= ratio;
 	yAxis *= ratio;
+	zAxis *= ratio;
 }
 
-void Point::rotation(double angle)
+
+double** Point::getXRotationMatrix(double angle)
+{
+	double** matrix = new double* [3];
+	for (int i = 0; i < 3; i++)
+		matrix[i] = new double[3];
+
+	matrix[0][0] = 1; matrix[0][1] = 0;          matrix[0][2] = 0;
+	matrix[1][0] = 0; matrix[1][1] = cos(angle); matrix[1][2] = -sin(angle);
+	matrix[2][0] = 0; matrix[2][1] = sin(angle); matrix[2][2] = cos(angle);
+	
+}
+
+double** Point::getYRotationMatrix(double angle)
+{
+	double** matrix = new double* [3];
+	for (int i = 0; i < 3; i++)
+		matrix[i] = new double[3];
+
+	matrix[0][0] = cos(angle);  matrix[0][1] = 0;          matrix[0][2] = sin(angle);
+	matrix[1][0] = 0;           matrix[1][1] = 1;          matrix[1][2] = 0;
+	matrix[2][0] = -sin(angle); matrix[2][1] = 0;          matrix[2][2] = cos(angle);
+
+}
+
+double** Point::getZRotationMatrix(double angle)
+{
+	double** matrix = new double* [3];
+	for (int i = 0; i < 3; i++)
+		matrix[i] = new double[3];
+
+	matrix[0][0] = cos(angle); matrix[0][1] = -sin(angle); matrix[0][2] = 0;
+	matrix[1][0] = sin(angle); matrix[1][1] = cos(angle);  matrix[1][2] = 0;
+	matrix[2][0] = 0;          matrix[2][1] = 0;           matrix[2][2] = 1;
+
+}
+
+void Point::rotate(double** matrix)
+{
+	double x = xAxis * matrix[0][0] + yAxis * matrix[0][1] + zAxis * matrix[0][2];
+	double y = xAxis * matrix[1][0] + yAxis * matrix[1][1] + zAxis * matrix[1][2];
+	double z = xAxis * matrix[2][0] + yAxis * matrix[2][1] + zAxis * matrix[2][2];
+
+	xAxis = x;
+	yAxis = y;
+	zAxis = z;	
+}
+
+void Point::Xrotation(double angle)
 {
 	angle *= (M_PI / 180);
-	float newXAwis = xAxis * cos(angle) - yAxis * sin(angle);
-	yAxis = xAxis * sin(angle) + yAxis * cos(angle);
-	xAxis = newXAwis;
+	rotate(getXRotationMatrix(angle));
 }
 
-
-float Point::rho()
+void Point::Yrotation(double angle)
 {
-	return sqrt(pow(xAxis, 2) + pow(yAxis, 2));
+	angle *= (M_PI / 180);
+	rotate(getYRotationMatrix(angle));
 }
 
-float Point::teta()
+
+void Point::Zrotation(double angle)
 {
-	if (xAxis == 0)
-		return 0;
-
-	return atan2(yAxis, xAxis);
+	angle *= (M_PI / 180);
+	rotate(getZRotationMatrix(angle));
 }
 
-void Point::move(int xOffset, int yOffset)
+
+
+
+void Point::translate(int xOffset, int yOffset, int zOffset)
 {
 	xAxis += xOffset;
 	yAxis += yOffset;
+	zAxis += zOffset;
 }
 
-void Point::display()
+Point& Point::operator+(Point& point)
+{
+	Point p(xAxis + point.xAxis,
+		yAxis + point.yAxis,
+		zAxis + point.zAxis);
+	return p;
+}
+
+bool Point::operator==(Point& point)
+{
+	if (xAxis == point.xAxis
+		&& yAxis == point.xAxis
+		&& zAxis == point.zAxis)
+		return true;
+	return false;
+}
+
+ostream& operator<<(ostream& stream, Point& point)
 {
 	cout << endl;
-	cout << "Coordonnee x : " << xAxis << endl;
-	cout << "Coordonnee y : " << yAxis << endl;
-	//cout << "Nombre de point : " << numberOfpoint 
+	cout << "Coordonnee x : " << point.xAxis << endl;
+	cout << "Coordonnee y : " << point.yAxis << endl;
+	cout << "Coordonnee z : " << point.zAxis << endl;
+	cout << "Couleur : " << point.color << endl;
 	cout << endl << endl;
 }
--- Draw3D/Point.h
+++ Draw3D/Point.h
@@ -1,32 +1,54 @@
 #pragma once
 #include <iostream>
 #include <math.h>
+#include "ColorDefinition.h"
 
 using namespace std;
 
 class Point
 {
+private :
+	double** getXRotationMatrix(double angle);
+	double** getYRotationMatrix(double angle);
+	double** getZRotationMatrix(double angle);
+
+	void rotate(double** matrix);
+
 protected:
 	float xAxis;
 	float yAxis;
 	float zAxis;
+	int color;
 
 
 public:
 	Point();
+	Point(float x, float y, float z);
+	Point(float x, float y, float z, int color);
+	Point(float x, float y, int color);
 	Point(float x, float y);
+	Point(Point& copy);
+
 
 	float getXaxis();
 	float getYaxis();
+	float getZaxis();
+	int   getColor();
 	void setXaxis(float x);
 	void setYaxis(float y);
+	void setZaxis(float z);
+	void setColor(int color);
+
 
 	void homotethia(double ratio);
-	void rotation(double angle);
-	float rho();
-	float teta();
-	void move(int xOffset, int yOffset);
+	void Xrotation(double angle);
+	void Yrotation(double angle);
+	void Zrotation(double angle);
+	void translate(int xOffset, int yOffset, int zOffset);
 
-	void display();
+	Point& operator+(Point& point);
+	bool operator==(Point& point);
+
+	friend ostream& operator<<(ostream& stream, Point& point);
 };
 
